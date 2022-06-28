ebf3da155c5926612cbca7dab463b5968fc209d1
KOSHKA1 <100477503+KOSHKA1@users.noreply.github.com>
2022-06-28 17:03:45 +0400

    js.app

---
+++ js/app.js
@@ -0,0 +1,31 @@
+
+
+
+
+
+let myArr2 = [{ name: "zuka", age: "18" , nickname : "musxela ",  }];
+
+console.log(myArr2);
+
+//ობიეტქი
+
+let person = {
+
+    firstName :"zuka",
+    lastName: "muskhelishvili",
+    address: ["tbilisi", "gldani"],
+    age: 18,
+    phoneNumbers : [123456,987654]
+
+};
+console.log(person);
+
+let firstAddress = person.address[0];
+
+
+
+
+
+let text = ` My name is ${person.firstName},My age is ${person.age},My address is ${firstAddress} `;
+
+console.log(text);
